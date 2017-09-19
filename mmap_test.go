7f67ad3ea1d843d58a39d94d1c8c85f86153f004
Deepak Jois <deepak.jois@gmail.com>
2017-09-19 13:00:56 +0530

    Trying with another file mode:

--- mmap_test.go
+++ mmap_test.go
@@ -3,19 +3,20 @@ package winmmap
 import "testing"
 import "os"
 import "github.com/dgraph-io/badger/y"
+import "math"
 
 func TestMmap(t *testing.T) {
 	t.Log("Trying mmap")
-	f, err := os.OpenFile("README.md", os.O_RDWR, 0777)
-	y.Check(err)
-	fi, err := f.Stat()
+	flags := os.O_RDWR | os.O_CREATE | os.O_EXCL
+	f, err := os.OpenFile("test.md", flags, 0666)
 	y.Check(err)
 	// size := int64(math.MaxUint32) FAILS!
 	// size := int64(500 * 1024 * 1024) FAILS!
 	// size := int64(10 * 1024 * 1024) FAILS!
-	size := fi.Size()
+	// size := fi.Size()
+	size := int64(math.MaxUint32)
 	t.Logf("Size is : %v", size)
-	_, err = trymmap(f, size+1)
+	_, err = trymmap(f, size)
 	if err != nil {
 		t.Errorf("mmap failed with error: %v", err)
 	}
