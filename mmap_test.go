dae26304ebd38fb8df37c6653c27fc3cc50d63e6
Deepak Jois <deepak.jois@gmail.com>
2017-09-19 10:33:49 +0530

    Trying size := 500MB

--- mmap_test.go
+++ mmap_test.go
@@ -3,7 +3,6 @@ package winmmap
 import "testing"
 import "os"
 import "github.com/dgraph-io/badger/y"
-import "math"
 
 func TestMmap(t *testing.T) {
 	t.Log("Trying mmap")
@@ -11,7 +10,8 @@ func TestMmap(t *testing.T) {
 	y.Check(err)
 	_, err = f.Stat()
 	y.Check(err)
-	size := int64(math.MaxUint32)
+	// size := int64(math.MaxUint32) FAILS!
+	size := int64(500 * 1024 * 1024)
 	_, err = trymmap(f, size)
 	if err != nil {
 		t.Errorf("mmap failed with error: %v", err)
