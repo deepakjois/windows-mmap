c0e4cb35874e9fbf386fb458b1abc2107b3f99c2
Deepak Jois <deepak.jois@gmail.com>
2017-09-20 09:22:10 +0530

    Try truncate after mmap

--- mmap_test.go
+++ mmap_test.go
@@ -8,6 +8,7 @@ func TestMmap(t *testing.T) {
 	t.Log("Trying mmap")
 	flags := os.O_RDWR | os.O_CREATE | os.O_EXCL
 	f, err := os.OpenFile("test.md", flags, 0666)
+	defer f.Close()
 	y.Check(err)
 	// size := int64(math.MaxUint32) FAILS!
 	size := int64(500 * 1024 * 1024)
@@ -18,5 +19,20 @@ func TestMmap(t *testing.T) {
 	_, err = trymmap(f, size)
 	if err != nil {
 		t.Errorf("mmap failed with error: %v", err)
+	}
+}
+
+func TestMmapAndTruncate(t *testing.T) {
+	f, err := os.OpenFile("README.md", os.O_RDWR, 0777)
+	defer f.Close()
+	y.Check(err)
+	fi, err := f.Stat()
+	y.Check(err)
+	size := fi.Size()
+	_, err = trymmap(f, size)
+	y.Check(err)
+	err = f.Truncate(size - 10)
+	if err != nil {
+		t.Errorf("truncate after mmap: %v", err)
 	}
 }
